0923aa6cab782ed81dc56cd292457cf380e72046
Alan Sheehan <alan.sheehan2@emc.com>
2017-03-02 10:55:40 +0000

    Setup project file structure

---
+++ smart-home-web-services/src/main/java/ie/sheehan/smarthome/SmartHomeWebServicesApplication.java
@@ -0,0 +1,12 @@
+package ie.sheehan.smarthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeWebServicesApplication.class, args);
+	}
+}
