c5a2032f42b9087212862464a225bcfb17997dbe
Alan Sheehan <al.j.sheehan@gmail.com>
2017-03-21 12:40:00 +0000

    Improved Android preferences style

--- android-app/app/src/main/java/ie/sheehan/smarthome/fragment/SettingsFragment.java
+++ android-app/app/src/main/java/ie/sheehan/smarthome/fragment/SettingsFragment.java
@@ -15,11 +15,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public static final String KEY_PREF_METRIC_TEMPERATURE = "pref_metric_temperature";
     public static final String KEY_PREF_METRIC_WEIGHT = "pref_metric_weight";
 
-    public static final String VALUE_METRIC_TEMPERATURE_CELSIUS = "0";
-    public static final String VALUE_METRIC_TEMPERATURE_FAHRENHEIT = "1";
+    public static final int VALUE_METRIC_TEMPERATURE_CELSIUS = 0;
+    public static final int VALUE_METRIC_TEMPERATURE_FAHRENHEIT = 1;
 
-    public static final String VALUE_METRIC_WEIGHT_METRIC = "0";
-    public static final String VALUE_METRIC_WEIGHT_IMPERIAL = "1";
+    public static final int VALUE_METRIC_WEIGHT_METRIC = 0;
+    public static final int VALUE_METRIC_WEIGHT_IMPERIAL = 1;
 
 
     public SettingsFragment() {
