5b554c2eb6604212884eac1245758ab525a9fb08
Alan Sheehan <al.j.sheehan@gmail.com>
2017-03-21 09:21:20 +0000

    Added initial settings functionality

--- android-app/app/src/main/java/ie/sheehan/smarthome/fragment/SettingsFragment.java
+++ android-app/app/src/main/java/ie/sheehan/smarthome/fragment/SettingsFragment.java
@@ -3,28 +3,28 @@ package ie.sheehan.smarthome.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import ie.sheehan.smarthome.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
 }
