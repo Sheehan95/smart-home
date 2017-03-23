981a76c55a58d7de8e457f1f1f0a88eaf23b22bc
Alan Sheehan <al.j.sheehan@gmail.com>
2017-03-23 19:59:49 +0000

    Refactored Pi components to be more in-line with the system

---
+++ raspi-components/smarthome/environment_component.py
@@ -0,0 +1,61 @@
+import json
+import time
+
+import paho.mqtt.client as mqtt
+
+from sensors import TemperatureSensor
+
+MQTT_BROKER = '192.167.1.23'
+
+TOPIC_ENVIRONMENT_READING_REQUESTS = '/ie/sheehan/smart-home/envreading/request'
+TOPIC_ENVIRONMENT_READING_RESPONSE = '/ie/sheehan/smart-home/envreading/response'
+
+
+mqtt_client = mqtt.Client()
+sensor = TemperatureSensor()
+
+
+def on_connect(client, userdata, flags, rc):
+    print 'Client connected with status code ', rc
+
+    if client is not None:
+        print 'Client: ', client
+
+    if userdata is not None:
+        print 'User Data: ', userdata
+
+    if flags is not None:
+        print 'Flags: ', flags
+
+
+def on_message(client, userdata, message):
+    print 'Client: ', client
+    print 'Topic: ', message.topic
+    print 'Payload: ', message.payload
+
+    if userdata is not None:
+        print 'User Data: ', userdata
+
+    if message.topic is TOPIC_ENVIRONMENT_READING_REQUESTS:
+        log_environment_reading(sensor.get_temp(), sensor.get_humidity())
+
+
+def log_environment_reading(temperature, humidity):
+    timestamp = int(time.time())
+
+    payload = json.dumps({'temperature': temperature, 'humidity': humidity, 'timestamp': timestamp})
+    mqtt_client.publish(TOPIC_ENVIRONMENT_READING_RESPONSE, payload)
+
+
+def main():
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_BROKER, 1883, 60)
+    mqtt_client.subscribe(TOPIC_ENVIRONMENT_READING_REQUESTS)
+
+    mqtt_client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
--- raspi-components/smarthome/gateway.py
+++ raspi-components/smarthome/gateway.py
@@ -3,34 +3,48 @@ import paho.mqtt.client as mqtt
 import requests
 
 
-ENVIRONMENT_TOPIC = '/ie/sheehan/smart-home/envreading/log'
-FORWARD_TEMPERATURE = '/ie/sheehan/smart-home/envreading/forward'
+MQTT_BROKER = '192.167.1.23'
+
+TOPIC_ENVIRONMENT_READING_LOG = '/ie/sheehan/smart-home/envreading/log'
+
+
+mqtt_client = mqtt.Client()
 
 
 def on_connect(client, userdata, flags, rc):
     print 'Client connected with status code ', rc
 
+    if client is not None:
+        print 'Client: ', client
+
+    if userdata is not None:
+        print 'User Data: ', userdata
+
+    if flags is not None:
+        print 'Flags: ', flags
+
 
 def on_message(client, userdata, message):
-
-    if message.topic == ENVIRONMENT_TOPIC:
-        payload = json.loads(message.payload)
-        request = requests.post('http://192.167.1.31:8080/environment/add', json=payload)
-        print request.status_code
-
     print 'Client: ', client
     print 'Topic: ', message.topic
     print 'Payload:', message.payload
 
+    if userdata is not None:
+        print 'User Data: ', userdata
+
+    if message.topic == TOPIC_ENVIRONMENT_READING_LOG:
+        payload = json.loads(message.payload)
+        request = requests.post('http://192.167.1.31:8080/environment/add', json=payload)
+        print request.status_code
+
 
 def main():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
 
-    client.connect('127.0.0.1', 1883, 60)
-    client.subscribe('#')
-    client.loop_forever()
+    mqtt_client.connect('127.0.0.1', 1883, 60)
+    mqtt_client.subscribe('#')
+    mqtt_client.loop_forever()
 
 
 if __name__ == '__main__':
