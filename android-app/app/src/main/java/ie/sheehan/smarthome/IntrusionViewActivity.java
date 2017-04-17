6b3ed91dc20919c9f5ef436166eff431989dfb84
Alan Sheehan <al.j.sheehan@gmail.com>
2017-04-17 22:39:31 +0100

    Improved Android GUI for intrusion-related views

--- android-app/app/src/main/java/ie/sheehan/smarthome/IntrusionViewActivity.java
+++ android-app/app/src/main/java/ie/sheehan/smarthome/IntrusionViewActivity.java
@@ -6,20 +6,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Date;
+
 import ie.sheehan.smarthome.model.IntrusionReading;
+import ie.sheehan.smarthome.utility.DateUtility;
 
 public class IntrusionViewActivity extends AppCompatActivity {
 
     ImageView imageView;
+    TextView dateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intrusion_view);
 
-        imageView = (ImageView) findViewById(R.id.break_in_image);
+        imageView = (ImageView) findViewById(R.id.image_intrusion);
+        dateView = (TextView) findViewById(R.id.text_label_intrusion_date);
 
         Bundle arguments = getIntent().getExtras();
 
@@ -29,6 +35,9 @@ public class IntrusionViewActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.toast_no_intrusion_selected, Toast.LENGTH_SHORT).show();
             return;
         }
+
+        String dateText = DateUtility.getDateFormat().format(new Date(intrusionReading.timestamp * 1000L));
+        dateView.setText(String.format(getResources().getString(R.string.text_label_intrusion_date), dateText));
 
         byte[] data = Base64.decode(intrusionReading.image, Base64.DEFAULT);
         Bitmap image = BitmapFactory.decodeByteArray(data, 0, data.length);
